171062e7b75c09b8cddda279ab0a32ad802b3e3e
vovalavr573 <vovalavr573@gmail.com>
2024-10-28 17:55:24 +0500

    Update 1.txt изучали css

---
+++ 5 занятие.js
@@ -0,0 +1,19 @@
+
+        let a = 1;
+        let b = "sadasdfs";
+        let c = true;
+        let d = null
+        let t =  a;
+        let r = a+t;
+        console.log("здесь я проверяю значение данных")
+        console.log (r);
+        console.log(r , a, t ,)
+        let hello="привет,"
+        let name="владимир"
+        let st= hello + name;
+        console.log(st)
+        console.log(hello + name)
+        console.log(a);
+        a = 5
+        console.log(a);
+        let span = getcument.getElementById(element)
