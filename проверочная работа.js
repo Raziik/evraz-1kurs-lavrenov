2a1f296bf55f42b76d715627007e36e3cde4fb35
vovalavr573 <vovalavr573@gmail.com>
2024-10-30 20:00:02 +0500

    Update 1.txt изучали css

---
+++ проверочная работа.js
@@ -0,0 +1,15 @@
+
+
+let name =  prompt ("укажите ваше имя")
+console.log(name)
+
+let surname = prompt("укажите вашу фамилию")
+console.log(surname)
+let patronimic = prompt("укажите ваше отчество")
+console.log(patronimic)
+let age = prompt("укажите ваш возраст")
+console.log(age)
+let school = prompt("укажите номер вашей школы")
+console.log(school)
+let klass = prompt("укажите ваш класс")
+console.log(klass)
