c6ebbfaddafc17b7060dc69f0f1304b2fe8e3f04
vovalavr573 <vovalavr573@gmail.com>
2024-11-13 20:00:08 +0500

    Update 1.txt изучали css

---
+++ 8 занятие n1.js
@@ -0,0 +1,14 @@
+
+function makepurple() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'purple';
+}
+function makeyellow() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'yellow';
+
+}
+function makeblue() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'blue';
+}
---
+++ 8 занятие.js
@@ -0,0 +1,13 @@
+let div = document.getElementById("div")
+console.log(div.innerText)
+div.innertext = "304506555"
+console.log(div.innertext)
+let  input = document.getElementById("input")
+console.log(input.value)
+input.value = "123123123"
+console.log(input.value);
+let s1 = document.getElementById("s1")
+let s2 = document.getElementById("s2")
+let result = s1 + s2;
+let res = document.getElementById("res")
+res.innertext = result;
