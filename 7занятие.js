c7f8433548cc76995dd75cfc88ada70482a89800
vovalavr573 <vovalavr573@gmail.com>
2024-11-20 19:57:36 +0500

    Update 1.txt изучали css

--- 7занятие.js
+++ 7занятие.js
@@ -3,3 +3,17 @@ function clearDIV() {
 console.log("вызываем очистку дива")
 div.innerText = "";
 }
+function makeRed() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'red';
+}
+
+function makeGreen() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'green';
+}
+
+function makeBlue() {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'blue';
+}
--- 8 занятие n1.js
+++ 8 занятие n1.js
@@ -1,4 +1,5 @@
 
+
 function makepurple() {
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'purple';
@@ -6,9 +7,24 @@ function makepurple() {
 function makeyellow() {
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'yellow';
-
 }
-function makeblue() {
+function makegreen() {
     let body = document.getElementById('sdf');
-    body.style.backgroundColor = 'blue';
+    body.style.backgroundColor = 'green';
 }
+
+let change = prompt ("изменить текст div ниже")
+let changeDOM = document.getElementById('change');
+changeDOM.innerText = change;
+console.log(change)
+{
+function () {
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = '';
+}
+
+
+function clearDiv() {
+    console.log('очистить всё');
+    let div = document.getElementById('clear');
+    div.innerText = 'clear';
