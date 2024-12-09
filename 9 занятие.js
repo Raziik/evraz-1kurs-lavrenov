391408b9e375f6739a72fc91f106f5f9223a17a4
vovalavr573 <vovalavr573@gmail.com>
2024-12-09 17:59:14 +0500

    Update 1.txt изучали css

--- 9 занятие.js
+++ 9 занятие.js
@@ -1,24 +1,56 @@
+// текущее значение
+let a = 0;
+
+let result = document.getElementById('res');
 
 function makeBlue() {
+    a = 2;
+    result.innerText = a;
     let body = document.getElementById('sdf');
-    body.style.backgroundColor = 'blue';}
+    body.style.backgroundColor = 'blue';
+}
 
-    function makegreen() {
-        let body = document.getElementById('sdf');
-        body.style.backgroundColor = 'green';
-    }
+function makegreen() {
+    a = 5;
+    result.innerText = a;
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'green';
+}
+
 function makeBlack() {
+    a = 0;
+    result.innerText = a;
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'black';
+    body.style.color = 'white';
 }
+
 function makeWhite() {
+    a = 1;
+    result.innerText = a;
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'white';
+    body.style.color = 'black';
 }
 
-let a = 1
-let b =2
+function plus() {
+    a = a+1
+    console.log(a)
+}
 
-
-
-
+function minus() {
+    a = a-1
+    console.log(a)
+}
+if (a % 25=== 0) {
+    console.log('число четное');
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'red';
+    body.style.color = 'black';
+}
+else {
+    console.log('число нечетное');
+    let body = document.getElementById('sdf');
+    body.style.backgroundColor = 'gold';
+    body.style.color = 'black';
+}
