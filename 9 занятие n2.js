0030e43f808be5f7d77335e48806150ac4fad5da
vovalavr573 <vovalavr573@gmail.com>
2024-12-11 19:57:24 +0500

    Update 1.txt изучали css

---
+++ 9 занятие n2.js
@@ -0,0 +1,8 @@
+function check() {
+    console.log("проверить число")
+    let input = document.getElementById("check")
+    div.innertext =""
+}
+
+
+
--- 9 занятие.js
+++ 9 занятие.js
@@ -8,6 +8,8 @@ function makeBlue() {
     result.innerText = a;
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'blue';
+    body.style.color = "white"
+
 }
 
 function makegreen() {
@@ -15,6 +17,7 @@ function makegreen() {
     result.innerText = a;
     let body = document.getElementById('sdf');
     body.style.backgroundColor = 'green';
+    body.style.color = "white"
 }
 
 function makeBlack() {
@@ -34,23 +37,63 @@ function makeWhite() {
 }
 
 function plus() {
-    a = a+1
+    a = a + 1
     console.log(a)
+    result.innerText = a;
+    let body = document.getElementById('sdf');
+
+    if (a === 0) {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else if (a === 1) {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    } else if (a === 5) {
+        body.style.backgroundColor = "green"
+        body.style.color = "white";
+    } else if (a === 2) {
+        body.style.backgroundColor = "blue"
+        body.style.color = "white"
+    } else if (a % 2 === 0) {
+        console.log(' четное');
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'red';
+        body.style.color = 'black';
+    } else {
+        console.log(' нечетное');
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'gold';
+        body.style.color = 'black';
+    }
 }
 
 function minus() {
-    a = a-1
+    a = a - 1
     console.log(a)
-}
-if (a % 25=== 0) {
-    console.log('число четное');
+    result.innerText = a;
     let body = document.getElementById('sdf');
-    body.style.backgroundColor = 'red';
-    body.style.color = 'black';
-}
-else {
-    console.log('число нечетное');
-    let body = document.getElementById('sdf');
-    body.style.backgroundColor = 'gold';
-    body.style.color = 'black';
+
+    if (a === 0) {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else if (a === 1) {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    } else if (a === 5) {
+        body.style.backgroundColor = "green"
+        body.style.color = "white";
+    } else if (a === 2) {
+        body.style.backgroundColor = "blue"
+        body.style.color = "white"
+    } else if (a % 2 === 0) {
+        console.log(' четное');
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'red';
+        body.style.color = 'black';
+    } else {
+        console.log(' нечетное');
+        let body = document.getElementById('sdf');
+        body.style.backgroundColor = 'gold';
+        body.style.color = 'black';
+    }
 }
