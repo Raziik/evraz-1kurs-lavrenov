19d86813d74778368d09cec5514f554e58ffce7a
vovalavr573 <vovalavr573@gmail.com>
2024-12-18 19:53:34 +0500

    Update 1.txt изучали css

--- 9 занятие n3.js
+++ 9 занятие n3.js
@@ -1,11 +1,19 @@
+let result = document.getElementById('res');
 function find() {
     console.log("найти наибольшое число или числа")
     let input = document.getElementById("input")
-    if (+input.value % 2 === 0) {
-        console.log('число четное');
+
+    if (+input.value % 2===0){
+        console.log('первое число больше второго');
     }
-    else {
-        console.log('');
+      else if (+input.value) {
+        console.log('второе число больше первого');
     }
+        else  {
+            console.log('превое число равно второму')
+    }
+
+
+
 
 }
