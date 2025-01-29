64c0725cc042ff0775afd37095ccfaad5954e98a
vovalavr573 <vovalavr573@gmail.com>
2025-01-29 19:59:37 +0500

    Update 1.txt изучали css

--- занятие 27-01-25.js
+++ занятие 27-01-25.js
@@ -7,16 +7,35 @@
 //     addElement.append(liElement)
 //     i =i + 1
 // }
-let i =name
+let i = name
+
 function addli() {
-    let addElement=document.getElementById('add')
-    let liElement=document.createElement('li')
+    let addElement = document.getElementById('add')
+    let liElement = document.createElement('li')
     liElement.innnetText = i
     addElement.append(liElement)
-    let input=document.getElementById('name')
-    if (input.value===''){
+    let input = document.getElementById('name')
+    if (input.value === '') {
         alert('добавить значение')
-    }
-    else{ liElement.innerText=input.value
-    }
+    } else
+        liElement.innerText = input.value
+
+
+}
+
+
+let b = name
+
+function addlI() {
+    let addElement = document.getElementById('add.2')
+    let liElement = document.createElement('lI')
+    liElement.innnetText = b
+    addElement.append(liElement)
+    let input = document.getElementById('name.2')
+    if (input.value === '') {
+        alert('добавить значение')
+    } else
+        liElement.innerText = input.value
+
+
 }
