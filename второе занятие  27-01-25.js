4c98ccd8b3990ab72865ac379a56be09366a5ca5
vovalavr573 <vovalavr573@gmail.com>
2025-02-17 18:00:23 +0500

    Update 1.txt изучали css

--- второе занятие  27-01-25.js
+++ второе занятие  27-01-25.js
@@ -1,35 +1,37 @@
-let a = name
+let a = 1
 
 function addli1() {
     let addElement = document.getElementById('add.1')
     let liElement = document.createElement('li')
     liElement.innnetText = a
     addElement.append(liElement)
+    a=a+1
     let input = document.getElementById('name1')
     if (input.value === '') {
         alert('добавить значение')
     } else
         liElement.innerText = input.value
-
-    for (let a=1; a<1000; a++ ) {
-        console.log(a)
-    }
 }
-let b = name
+
+for (let a= 0; a < 25; a++ ) {
+    console.log(a+1,'')
+}
+
+let b = 1
 
 function addli2() {
     let addElement = document.getElementById('add.2')
     let liElement = document.createElement('li')
     liElement.innnetText = b
     addElement.append(liElement)
+    b=b+1
     let input = document.getElementById('name3')
     if (input.value === '') {
         alert('добавить значение')
     } else
         liElement.innerText = input.value
-    for (let b= 1; b < 1000; b++ ) {
-        console.log(b+1)
-    }
+
 }
-
-
+for (let b= 0; b < 25; b++ ) {
+    console.log(b+1,'')
+}
