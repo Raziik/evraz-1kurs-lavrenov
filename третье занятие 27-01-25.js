21368da9a199419fdbd4f2a1ebf9a3cc7bf68a45
vovalavr573 <vovalavr573@gmail.com>
2025-03-05 20:00:15 +0500

    Update 1.txt изучали css

---
+++ третье занятие 27-01-25.js
@@ -0,0 +1,27 @@
+function  addli1(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli2(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli3(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli4(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli5(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli6(){
+    let addElement = document.getElementById('add.1')
+}
+
+function  addli7(){
+    let addElement = document.getElementById('add.1')
+}
